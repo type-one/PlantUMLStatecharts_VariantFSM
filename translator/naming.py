470fc86b18cd59030d07906b922f3042c09aef01
Laurent Lardinois <19912792+type-one@users.noreply.github.com>
2026-03-31 22:10:59 +0200

    refactor: add license banners in py files

--- translator/naming.py
+++ translator/naming.py
@@ -1,3 +1,22 @@
+###############################################################################
+## PlantUML Statecharts (State Machine) Translator.
+## Copyright (c) 2022 Quentin Quadrat <lecrapouille@gmail.com>
+##
+## This file is part of PlantUML Statecharts (State Machine) Translator.
+##
+## This tool is free software: you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see http://www.gnu.org/licenses/.
+###############################################################################
 """Naming helpers used by the translator.
 
 This module intentionally contains only pure naming utilities so they can be
